8125105ac15c7256dfe2a639b703cabf4bd14380
Hugo <hugo.carregno@gmail.com>
2019-08-27 17:05:19 -0300

    se agregaron permisos

--- app/src/main/java/com/example/trabajopractico1/MainActivity.java
+++ app/src/main/java/com/example/trabajopractico1/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.trabajopractico1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MensajeRecibido mr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.mr = new MensajeRecibido();
+        registerReceiver(this.mr, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(mr);
+        super.onPause();
+
+    }
+
 }
