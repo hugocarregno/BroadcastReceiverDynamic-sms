c15b0fbdd26653f159dc323b4b755919d28dc1fd
Hugo <hugo.carregno@gmail.com>
2019-08-28 12:34:55 -0300

    se agrego permiso explicito en el MainActivity

--- app/src/main/java/com/example/trabajopractico1/MainActivity.java
+++ app/src/main/java/com/example/trabajopractico1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.trabajopractico1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, 1000);
+        }
     }
 
     @Override
